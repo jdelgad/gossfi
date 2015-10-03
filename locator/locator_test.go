77a579f74890789eca1f712a49ea1e9ba50d6d18
Jacob Delgado <jacob.delgado@gmail.com>
2015-10-03 03:15:37 -0600

    moved unit test files to proper location

--- locator/locator_test.go
+++ locator/locator_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestTxtFiles(t *testing.T) {
-	f := FindTextFiles("unit_test/files", ".txt")
+	f := FindTextFiles("../unit_test/files", ".txt")
 	fmt.Printf("%s\n", f)
 	assert.Equal(t, len(f), 5)
 }
