a1b55fe358bb14c860ff4b13138842bfc29538f2
Jacob Delgado <jacob.delgado@gmail.com>
2015-10-10 00:35:07 -0600

    added reversal mapping

--- accumulator/accumulator.go
+++ accumulator/accumulator.go
@@ -1,6 +1,28 @@
 package accumulator
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
+
+type WordCountPair struct {
+	Word  string
+	Count int
+}
+
+type WordCount []WordCountPair
+
+func (wc WordCount) Len() int {
+	return len(wc)
+}
+
+func (wc WordCount) Less(i, j int) bool {
+	return wc[i].Count < wc[j].Count
+}
+
+func (wc WordCount) Swap(i, j int) {
+	wc[i], wc[j] = wc[j], wc[i]
+}
 
 var words map[string]int
 
@@ -15,4 +37,15 @@ func Accumulate(wdCh chan string, closeCh chan bool) {
 			return
 		}
 	}
+}
+
+func OrderWordsByCount() WordCount {
+	w := make(WordCount, len(words))
+	i := 0
+	for word, count := range words {
+		w[i] = WordCountPair{word, count}
+		i++
+	}
+	sort.Sort(sort.Reverse(w))
+	return w
 }
--- accumulator/accumulator_test.go
+++ accumulator/accumulator_test.go
@@ -32,3 +32,44 @@ func TestAccumulate(t *testing.T) {
 
 	assert.Equal(t, wMap, words)
 }
+
+func TestOrderWordsByCount(t *testing.T) {
+	words = make(map[string]int)
+	words["hi"] = 1
+	words["boom"] = 2
+	words["yes"] = 5
+
+	wc := make(WordCount, 3)
+	wc[0] = WordCountPair{"yes", 5}
+	wc[1] = WordCountPair{"boom", 2}
+	wc[2] = WordCountPair{"hi", 1}
+
+	wordCount := OrderWordsByCount()
+	assert.Equal(t, wordCount, wc)
+}
+
+func TestOrderWordsByCountEmpty(t *testing.T) {
+	words = make(map[string]int)
+
+	wc := make(WordCount, 0)
+
+	wordCount := OrderWordsByCount()
+	assert.Equal(t, wordCount, wc)
+}
+
+func TestOrderWordsByCountEquality(t *testing.T) {
+	words = make(map[string]int)
+	words["hi"] = 1
+	words["boom"] = 2
+	words["ok"] = 2
+	words["yes"] = 5
+
+	wc := make(WordCount, 4)
+	wc[0] = WordCountPair{"yes", 5}
+	wc[1] = WordCountPair{"boom", 2}
+	wc[2] = WordCountPair{"ok", 2}
+	wc[3] = WordCountPair{"hi", 1}
+
+	wordCount := OrderWordsByCount()
+	assert.Equal(t, wordCount, wc)
+}
